378a5dba7a2b6208663564a15a236fb94084d7f4
Riley Wilkinson <quintbro@gmail.com>
2023-11-15 11:29:35 -0700

    start setup.py file

---
+++ pyclimb/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'pyclimb',
+    version = '0.0.1',
+    description = 'A package to scrape, clean, and vizualize data from mountainproject.com',
+    author = 'Hayden Crofts and Riley Wilkinson',
+    author_email = 'rileyw@byu.edu',
+    url = 'https://github.com/quintbro/Climbing_and_Climate'
+)
