1be9092f392663c626ffa1a39ca0fa113fab9442
Riley Wilkinson <quintbro@gmail.com>
2023-11-27 10:46:31 -0700

    Add docstring to the function and test it out

--- pyclimb/temp_merge_file.py
+++ pyclimb/temp_merge_file.py
@@ -2,6 +2,20 @@ import pandas as pd
 from scipy.spatial import cKDTree
 
 def merge_data_dist(df1, df2, keep_col, lat = 'Latitude', lon = 'Longitude'):
+    '''
+    df1: Left data frame that you want to join 
+    NOTE: this will perform a left join, meaning that all of the columns in df1 will
+    be kept but not all of the columns in df2 will necessarily be kept
+
+    df2: Right data frame that you want to join
+    
+    keep_col: name of the column from df2 that you want to join to df1
+
+    lat and lon: the name of the latitude and longitude column for df2
+    NOTE: This assumes that df1 has column names "Latitude" and "Longitude"
+    respectively. Otherwise, the name of those columns must be the same names as
+    df2 which is specified by the lat and lon arguements
+    '''
     # Rename the df to correct column names
     df1 = df1.rename(columns={lat: 'Latitude', lon: 'Longitude'})
     df2 = df2.rename(columns={lat: 'Latitude', lon: 'Longitude'})
