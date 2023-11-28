02bb6087ac79ee3483056f05d8ab9338130ae5ca
Riley Wilkinson <quintbro@gmail.com>
2023-11-28 09:27:23 -0700

    put example in the doc string for the function

--- pyclimb/scrape_climbing.py
+++ pyclimb/scrape_climbing.py
@@ -21,6 +21,8 @@ def findyear(soup):
 
 def scrape_mp(df, crawl_delay = 60, inplace = False):
     '''
+    Parameters
+    ============
     df : This should be a dataframe with a column that includes URLs to specific
     climbs from mountainproject.com. This can be obtained by going to "route finder" on
     mountain project and exporting a csv file of the climbs you select
@@ -32,11 +34,47 @@ def scrape_mp(df, crawl_delay = 60, inplace = False):
     inplace : Default is False, if set to True, the dataframe that is passed in will 
     be modified
 
+    Description
+    =============
     This Function scrapes data from mountainproject.com, specifically, it creates 8
     different columns namely 'numVotes', 'numViews', 'Year', 'ViewsPerMonth', 
     'Shared_by', 'Month', 'Day', 'Date'
 
-    
+    Example
+    ============
+    >>> from pyclimb.clean_climbing import dataConcat
+    >>> from pyclimb.scrape_climbing import scrape_mp
+
+    >>> climbs = dataConcat(["route-finder_1.csv", "route-finder_2.csv"])
+    >>> test = scrape_mp(climbs.iloc[:2], crawl_delay = 2)
+    >>> test.info()
+    <class 'pandas.core.frame.DataFrame'>
+      RangeIndex: 2 entries, 0 to 1
+      Data columns (total 20 columns):
+      #   Column          Non-Null Count  Dtype         
+      ---  ------          --------------  -----         
+      0   index           2 non-null      int64         
+      1   Route           2 non-null      object        
+      2   Location        2 non-null      object        
+      3   URL             2 non-null      object        
+      4   Avg Stars       2 non-null      float64       
+      5   Your Stars      2 non-null      int64         
+      6   Route Type      2 non-null      object        
+      7   Rating          2 non-null      object        
+      8   Pitches         2 non-null      int64         
+      9   Length          2 non-null      float64       
+      10  Area Latitude   2 non-null      float64       
+      11  Area Longitude  2 non-null      float64       
+      12  numVotes        2 non-null      int32         
+      13  numViews        2 non-null      int32         
+      14  Year            2 non-null      object        
+      15  ViewsPerMonth   2 non-null      int32         
+      16  Shared_by       2 non-null      int64         
+      17  Month           2 non-null      int32         
+      18  Day             2 non-null      int32         
+      19  Date            2 non-null      datetime64[ns]
+      dtypes: datetime64[ns](1), float64(4), int32(5), int64(4), object(6)
+      memory usage: 412.0+ bytes
     '''
     if inplace == False:
         climbs = df.copy()
