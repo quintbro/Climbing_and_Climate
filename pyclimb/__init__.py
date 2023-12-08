1edb1994d5d46225f97637814d6f2ae85cafc7f2
Riley Wilkinson <quintbro@gmail.com>
2023-12-07 19:51:40 -0700

    update init.py

--- pyclimb/__init__.py
+++ pyclimb/__init__.py
@@ -1,3 +1,4 @@
 from pyclimb.load_data import load_data
 from pyclimb.clean_climbing import concat, clean
 from pyclimb.scrape_climbing import scrape_mp
+from pyclimb.data_merge import merge_data_dist
